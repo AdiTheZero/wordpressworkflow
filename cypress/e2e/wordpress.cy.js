196580fe43fffe41b0ebab6273d94f8b281a320e
aditya001 <adityakumarspn555@gmail.com>
2024-12-08 19:12:01 +0530

    reports

--- cypress/e2e/wordpress.cy.js
+++ cypress/e2e/wordpress.cy.js
@@ -6,21 +6,21 @@ describe('template spec', () => {
 
     /* ==== Generated with Cypress Studio ==== */
     cy.get('#language-continue').click();
-    cy.get('#weblog_title').clear('g');
+    cy.get('#weblog_title').clear();
     cy.get('#weblog_title').type('google');
-    cy.get('#user_login').clear('a');
+    cy.get('#user_login').clear();
     cy.get('#user_login').type('adityakumarspn');
-    cy.get('#pass1').clear('K');
+    cy.get('#pass1').clear();
     cy.get('#pass1').type('Adity@1234');
     cy.get('.pw-checkbox').check();
-    cy.get('#admin_email').clear('A');
+    cy.get('#admin_email').clear();
     cy.get('#admin_email').type('Adityakumarspn555@gmail.com');
     cy.get('#blog_public').check();
     cy.get('#submit').click();
     cy.get('a').click();
-    cy.get('#user_login').clear('a');
+    cy.get('#user_login').clear();
     cy.get('#user_login').type('adityakumarspn');
-    cy.get('#user_pass').clear('A');
+    cy.get('#user_pass').clear();
     cy.get('#user_pass').type('Adity@1234');
     cy.get('.dashicons').click();
     cy.get('#wp-submit').click();
