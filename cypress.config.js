12370dfe4817a91810d8910339a1d17e8d1e3220
aditya001 <adityakumarspn555@gmail.com>
2024-12-08 16:29:38 +0530

    m

--- cypress.config.js
+++ cypress.config.js
@@ -3,8 +3,15 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   e2e: {
     experimentalStudio: true,
+    reporter: 'mochawesome',
+    reporterOptions: {
+      reportDir: 'cypress/reports', // Directory for reports
+      overwrite: false, // Keep old reports
+      html: true, // Generate HTML reports
+      json: true, // Generate JSON reports
+    },
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      // Implement node event listeners here
     },
   },
 });
